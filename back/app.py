9af9f9c6c19c50637a6fed362d2b931140287d03
franckcwalter <111013607+franckcwalter@users.noreply.github.com>
2024-10-02 21:48:53 +0200

    add views + header +navigation (#2)
    Co-authored-by: franck walter <fwalter@dev-id.fr>

--- back/app.py
+++ back/app.py
@@ -1,14 +1,24 @@
-# save this as app.py
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates 
 
 app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
 
-@app.get("/", response_class=HTMLResponse)
-async def read_item(request: Request):
-    title = "Hello from FastAPI"
-    message = "Hello World !"
-    return templates.TemplateResponse("index.html", {"request": request, "title": title, "message": message})
+@app.get("/", response_class=RedirectResponse)
+async def root():
+    return RedirectResponse(url="/onboarding")
+
+@app.get("/onboarding", response_class=HTMLResponse)
+async def onboarding(request: Request):
+    return templates.TemplateResponse("onboarding.html", {"request": request})
+
+@app.get("/search", response_class=HTMLResponse)
+async def search(request: Request):
+    return templates.TemplateResponse("search.html", {"request": request})
+
+@app.get("/result", response_class=HTMLResponse)
+async def result(request: Request):
+    return templates.TemplateResponse("result.html", {"request": request})
+
