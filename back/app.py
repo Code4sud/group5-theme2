7135069429bca3d7f2c697fcb8125e03f152062a
Nassima <136086372+Nasargus@users.noreply.github.com>
2024-10-03 15:38:10 +0200

    base onboarding (#3)

--- back/app.py
+++ back/app.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI, Request, Depends, Form, HTTPException
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates 
 from sqlalchemy.orm import Session
+from fastapi.staticfiles import StaticFiles
 from db.database import SessionLocal, engine
 from db import crud , database, models, schemas
 
@@ -19,17 +20,22 @@ def get_db():
         db.close()
 
 
+
 templates = Jinja2Templates(directory="templates")
+# Monte le répertoire 'templates/header/images' en tant que source de fichiers statiques
+# à l'URL '/header/images'.
+# Cela permet d'accéder aux fichiers situés dans ce répertoire via une URL,
+# par exemple, pour récupérer une image, on peut utiliser : 
+# http://localhost:8000/header/images/nom_de_l_image.ext
+#  
+app.mount("/header/images", StaticFiles(directory="templates/header/images"), name="images")
+
 
 @app.get("/", response_class=RedirectResponse)
 async def root():
     return RedirectResponse(url="/onboarding")
 
-@app.get("/onboarding", response_class=HTMLResponse)
-async def onboarding(request: Request):
-    return templates.TemplateResponse("onboarding.html", {"request": request})
-
-
+# page de recherche
 @app.get("/search", response_class=HTMLResponse)
 async def search(request: Request):
     return templates.TemplateResponse("search.html", {"request": request})
@@ -53,3 +59,19 @@ def create_simple_user(
     user = crud.create_simpleuser(db, name=name, city=city)
     print(user.city, user.name, user.id)
     return RedirectResponse(url="/search", status_code=303)
+
+# @app.get("/get-picture")
+# def get_picture(
+
+# )
+
+@app.get("/onboarding", response_class=HTMLResponse)
+async def onboarding(request: Request):
+# Définition du chemin d'accès à l'image 'arbre1.jpeg' qui se trouve dans le répertoire
+# monté pour les fichiers statiques. Ce chemin sera utilisé pour afficher l'image
+# dans le template HTML.
+    image_file = "/header/images/arbre1.jpeg"  
+# Retourne une réponse au format HTML en utilisant le template 'onboarding.html'.
+# On passe un dictionnaire contenant l'objet 'request' (qui représente la requête HTTP actuelle)
+# et 'image_file' (le chemin de l'image) afin qu'ils soient accessibles dans le template.
+    return templates.TemplateResponse("onboarding.html", {"request": request, "image_file": image_file})
